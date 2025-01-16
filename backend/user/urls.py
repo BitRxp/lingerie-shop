3216795c05ba091539a60adb8039b5ef2fd96f30
Alex <oleksandrtaranenko239@gmail.com>
2025-01-16 14:58:30 +0200

    updated endpoint name

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -8,7 +8,7 @@ from .views import CreateUserView, ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create-user"),
-    path("me/", ManageUserView.as_view(), name="manage-user"),
+    path("profile/", ManageUserView.as_view(), name="manage-user"),
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
