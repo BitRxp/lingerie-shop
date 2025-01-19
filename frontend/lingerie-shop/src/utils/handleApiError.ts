4d4fd2099f49cd42f9e31de630074002b4a53613
Alex <oleksandrtaranenko239@gmail.com>
2025-01-19 22:40:52 +0200

    Удаление пÐ

--- frontend/lingerie-shop/src/utils/handleApiError.ts
+++
@@ -1,5 +0,0 @@
-export const handleApiError = (error: any) => {
-    console.error('API Error:', error.response?.data || error.message);
-    alert('An error occurred while processing your request.');
-  };
-  
