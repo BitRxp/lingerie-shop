19eb0bd9b268e6b76d3e1e41450bf1b2a2f3f7d9
Oleksandr Taranenko <105813721+BitRxp@users.noreply.github.com>
2025-01-19 22:44:38 +0200

    Delete frontend directory

--- frontend/lingerie-shop/src/services/ProductService.ts
+++
@@ -1,8 +0,0 @@
-import axiosInstance from '../utils/axiosInstance';
-
-export const getNewCollection = () => axiosInstance.get('/products/new');
-export const getBestSellers = () => axiosInstance.get('/products/bestsellers');
-export const getProductsOnSale = () => axiosInstance.get('/products/sale');
-export const getTailoringProducts = () => axiosInstance.get('/products/tailoring');
-export const searchProducts = (filters: any) => axiosInstance.post('/products/search', filters);
-export const getProductById = (id: number) => axiosInstance.get(`/products/${id}`);
